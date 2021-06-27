0cf509b125833e232dbf3916a19c2ac3a4bd8f7a
gulam101 <gfram@hotmail.co.uk>
2021-06-27 20:52:37 +0100

    Updated Method

--- script.js
+++ script.js
@@ -7,7 +7,22 @@ const app = Vue.createApp({
             gender: 'male',
             picture: 'https://randomuser.me/api/portraits/men/82.jpg',
         }
-    }
+    },
+    methods: {
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+
+            console.log(results)
+
+            // using the 'This' keyword allows us to access the data
+            this.firstName = 'Sam'
+            this.lastName = 'Smith'
+            this.email = 'Sam@gmail.com'
+            this.gender = 'female'
+            this.picture = 'https://randomuser.me/api/portraits/women/82.jpg'
+        },
+    },
 })
 
 app.mount('#script')
