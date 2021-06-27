8392265817c3df2f8691a64b97af27a48a377f5b
gulam101 <gfram@hotmail.co.uk>
2021-06-27 22:02:47 +0100

    Fixed API Issue, Corrected Typo + Added Age

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
             email: 'john@gmail.com',
             gender: 'male',
             picture: 'https://randomuser.me/api/portraits/men/82.jpg',
+            DoB: '41',
         }
     },
     methods: {
@@ -14,15 +15,19 @@ const app = Vue.createApp({
             const res = await fetch('https://randomuser.me/api')
             const { results } = await res.json()
 
+            console.log(results)
 
             // using the 'This' keyword allows us to access the data
             this.firstName = results[0].name.first
             this.lastName = results[0].name.last
-            this.email = results[0].name.email
-            this.gender = results[0].name.gender
-            this.picture = results[0].name.picture
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+            this.DoB = results[0].dob.age
         },
     },
+
+    
 })
 
 app.mount('#script')
